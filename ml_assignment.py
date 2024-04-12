65425ab584090aeb32192efc2f51745d86193df8
RushikeshChary <ruushid19@gmail.com>
2024-04-12 23:36:11 +0530

    final commit

--- ml_assignment.py
+++ ml_assignment.py
@@ -6,6 +6,8 @@ from sklearn.metrics import accuracy_score, classification_report
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import f1_score
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.preprocessing import LabelEncoder
 
 # Function to convert monetary values to numeric
 def convert_to_float(crore_str):
@@ -31,21 +33,7 @@ data = pd.read_csv('train.csv')
 data_test = pd.read_csv('test.csv')
 
 # Explore the data
-print(data.head())
-
-# Remove leading/trailing spaces from column names
-data.columns = data.columns.str.strip()
-data_test.columns = data_test.columns.str.strip()
-
-# Apply conversion function to monetary columns (handling potential errors)
-try:
-    data['Total Assets'] = data['Total Assets'].apply(convert_to_float)
-    data['Liabilities'] = data['Liabilities'].apply(convert_to_float)
-    data_test['Total Assets'] = data_test['Total Assets'].apply(convert_to_float)
-    data_test['Liabilities'] = data_test['Liabilities'].apply(convert_to_float)
-except:
-    print("Error occurred during conversion. Check data format!")
-
+# print(data.head())
 # # Plot representing the percentage of candidates with criminal records.*********************************************************
 
 # Filter the dataset to include only candidates with criminal records
@@ -64,8 +52,8 @@ plt.xlabel('Percentage of Candidates with Criminal Records')
 plt.ylabel('Party')
 plt.title('Percentage Distribution of Parties with Candidates Having Criminal Records')
 plt.show()
-# *******************************************************************************************************************************
-# Plot representing the party's percentage wealth.***************************************************************************
+
+# # # Plot representing the party's percentage wealth.***************************************************************************
 wealthy_candidates_data = data[data['Total Assets'] > 0]
 
 # Group the data by party and calculate the total declared wealth for candidates in each party
@@ -81,36 +69,49 @@ plt.ylabel('Party')
 plt.title('Percentage Distribution of Parties with the Most Wealthy Candidates')
 plt.show()
 # ***********************************************************************************************************************************
-# Drop columns (if desired)
+
+# Remove leading/trailing spaces from column names
+data.columns = data.columns.str.strip()
+data_test.columns = data_test.columns.str.strip()
+
+# Extract data within brackets in the 'Constituency' column and encode them.
+data['Constituency'] = data['Constituency ∇'].str.extract(r'\((.*?)\)').fillna('NONE')
+data_test['Constituency'] = data_test['Constituency ∇'].str.extract(r'\((.*?)\)').fillna('NONE')
+label_encoder = LabelEncoder()
+data['Constituency'] = label_encoder.fit_transform(data['Constituency'])
+data_test['Constituency'] = label_encoder.transform(data_test['Constituency'])
+
+# Apply conversion function to monetary columns (handling potential errors)
+try:
+    data['Total Assets'] = data['Total Assets'].apply(convert_to_float)
+    data['Liabilities'] = data['Liabilities'].apply(convert_to_float)
+    data_test['Total Assets'] = data_test['Total Assets'].apply(convert_to_float)
+    data_test['Liabilities'] = data_test['Liabilities'].apply(convert_to_float)
+except:
+    print("Error occurred during conversion. Check data format!")
+
+# Drop some columns
 data['tot_revenue'] = data['Total Assets'] - data['Liabilities']
 data_test['tot_revenue'] = data_test['Total Assets'] - data_test['Liabilities']
-drop_cols = ['ID', 'Candidate', 'Constituency ∇', 'Education']  # Example columns to drop
+drop_cols = ['ID', 'Candidate', 'Constituency ∇', 'Education','Total Assets','Liabilities']
 X = data.drop(columns=drop_cols)
-X_test = data_test.drop(columns=['ID', 'Candidate', 'Constituency ∇'])
+X_test = data_test.drop(columns=['ID', 'Candidate', 'Constituency ∇','Total Assets', 'Liabilities'])
 y = data['Education']
 
 # Encode categorical variables
 X = pd.get_dummies(X, columns=['Party', 'state'])
 X_test = pd.get_dummies(X_test, columns=['Party', 'state'])
 
-# Split data into train and test sets
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2024)
 
-best_params = best_params ={'max_depth': None, 'max_features': 'log2', 'min_samples_leaf': 1, 'min_samples_split': 5, 'n_estimators': 100}
-# Train model with best parameters on entire training data
-best_rf_classifier = RandomForestClassifier(random_state=42, **best_params)
-best_rf_classifier.fit(X, y)
+# Model implementation
+model = BernoulliNB(alpha=0.5, binarize=0.1,fit_prior=True,class_prior=None)
+model.fit(X, y)
 
-# Predictions on the test data
-y_pred = best_rf_classifier.predict(X_test)
+# # # Predictions
+y_pred = model.predict(X_test)
 
-# Convert predictions to a DataFrame with 'ID' column from the test dataset
 predictions_df = pd.DataFrame({'ID': np.arange(len(y_pred)), 'Education': y_pred})
 
-# Write predictions to a CSV file
-predictions_df.to_csv('predictions.csv', index=False)
-
-# Evaluation
-# print("Accuracy:", accuracy_score(y_test, y_pred))
-# Calculate F1 score
-# print("F1 Score:", f1_score(y_test, y_pred, average='weighted'))
+# # # Write predictions to a CSV file
+predictions_df.to_csv('final_predictions.csv', index=False)
